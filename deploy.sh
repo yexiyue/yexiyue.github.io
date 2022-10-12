fe15e827b20c2a86bcd9b11a2251913bedd125a4
yexiyue <yexiyue666@qq.com>
2022-10-12 11:03:17 +0800

    脚本自动上传

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git status
 git add .
 git commit -m "脚本自动上传"
 #下面配置你的github地址，中间最长的哪里替换复制下来的链接
-git push git@github.com:yexiyue/yexiyue.github.io.git master
+git push https://github.com/yexiyue/yexiyue.github.io.git master
 cd ./dist
 ls
 echo -e "\033[1;32m========开始执行git相关操作========\033[0m"
@@ -15,5 +15,5 @@ git status
 git add -A
 git commit -m "deploy"
 #下面配置你的github地址，中间最长的哪里替换复制下来的链接
-git push -f git@github.com:yexiyue/yexiyue.github.io.git master:gh-pages
+git push -f https://github.com/yexiyue/yexiyue.github.io.git master:gh-pages
 echo -e "\033[1;32m========推送成功=======\033[0m"
